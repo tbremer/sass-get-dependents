87f48c355d531959aa6afcf81c049600a5c832e5
Tom Bremer <tom@tbremer.com>
2014-08-25 18:47:25 -0500

    push with some tests.

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+ * sass-partial-dependencies
+ * https://github.com/tbremer/sass-partial-dependencies
+ *
+ * Copyright (c) 2014 Thomas Bremer, contributors
+ * Licensed under the MIT license.
+ */
+'use strict';
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(needle, haystack) {
+	if (haystack==='undefined') {
+		console.log('nope');
+	}
+
+	// Check if file exists | return false
+
+	// Check that file isn't *.css | return false
+
+	// Build relative path
+
+	// Look for relative path in file(s).
+		// push to array of files
+	// Return array of files
+}
