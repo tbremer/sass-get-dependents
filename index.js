fc651ce521e5ddf00a5f6048ace65a543d1d6519
Tom Bremer <tom@tbremer.com>
2014-08-26 01:38:35 -0500

    jshint fixes. stable, but tomorrow we write tests

--- index.js
+++ index.js
@@ -5,25 +5,47 @@
  * Copyright (c) 2014 Thomas Bremer, contributors
  * Licensed under the MIT license.
  */
-'use strict';
-var fs = require('fs');
-var path = require('path');
-var chalk = require('chalk');
+ 'use strict';
+ var fs = require('fs');
+ var path = require('path');
+ var chalk = require('chalk');
+ var glob = require('glob');
+ var _ = require('underscore');
 
-module.exports = function(needle, haystack) {
-	console.log(chalk.red.bhWhite('hello'));
-	console.log(chalk.red.bgWhite(needle));
-	if (haystack==='undefined') {
-		console.log('nope');
+ module.exports = function(needle, haystack) {
+	// Check if file exists | return false
+	if (!fs.existsSync(needle)) {
+		console.log(chalk.white.bgRed('File doesn\'t exist.'));
+		return false;
+	}
+	if (path.extname(needle) === '.css') {
+		console.log(chalk.white.bgRed('SASS won\'t import *.css files'));
+		return false;
+	}
+	var dependentFiles = [],
+	ext = path.extname(needle);
+
+	if (haystack === undefined) {
+		haystack = glob.sync('**/*' + ext);
 	}
 
-	// Check if file exists | return false
+	haystack = _.without(haystack, needle);
+	_.each(haystack, function(straw) {
+		var thisContents = fs.readFileSync(straw, {encoding: 'utf8'});
+		var relPath = path.relative(straw, needle).replace('../', '').replace('/_','/').replace(ext, '');
+		if(relPath.substring(0,1) === '_') {
+			relPath.replace('_','');
+		}
+		var isDependent = thisContents.indexOf(relPath) !== -1 ? true : false;
+		if(isDependent) {
+			var isPartial = path.basename(straw)[0] === '_' ? true : false;
+			if(isPartial) {
+				haystack.push(straw);
+			} else {
+				dependentFiles.push(straw);
+			}
+		}
+	});
 
-	// Check that file isn't *.css | return false
-
-	// Build relative path
-
-	// Look for relative path in file(s).
-		// push to array of files
-	// Return array of files
-}
+	return dependentFiles;
+};
