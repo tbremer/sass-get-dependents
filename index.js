272062c749276dcfd1fabaed5e072f4359a9e5b5
Tom Bremer <tom@tbremer.com>
2014-08-31 23:14:01 -0500

    add in tests, allow package to be run as a bin file, update the code base

--- index.js
+++ index.js
@@ -1,82 +1,135 @@
+#!/usr/local/bin/node
+
 /*
- * sass-partial-dependencies
- * https://github.com/tbremer/sass-partial-dependencies
+ * sass-get-dependencies
+ * https://github.com/tbremer/sass-get-dependencies
  *
  * Copyright (c) 2014 Thomas Bremer, contributors
  * Licensed under the MIT license.
- */
- 'use strict';
- var fs = require('fs');
- var path = require('path');
- var chalk = require('chalk');
- var glob = require('glob');
- var _ = require('underscore');
+*/
+'use strict';
+var startTime = Date.now();
+var _ = require('/Users/tom.bremer/vagrant/www/gruntjs/stable/underscore');
+var chalk = require('/Users/tom.bremer/vagrant/www/gruntjs/stable/chalk');
+var fs = require('fs');
+var glob = require('/Users/tom.bremer/vagrant/www/gruntjs/stable/glob');
+var path = require('path');
 
- module.exports = function(needle, haystack) {
- 	var timerStart = Date.now();
- 	var isVerbose = (process.argv.indexOf('--verbose') !== -1 || process.argv.indexOf('-v') !== -1) ? true : false;
 
-	// Check if file exists | return false
-	if (!fs.existsSync(needle)) {
-		if (isVerbose) {
-			console.log(chalk.cyan('File doesn\'t exist.'));
+// does needle exist ✓
+// is needle a valid file type ✓
+// readFile
+// does read have an import
+// does import match relpath
+// build
+
+var escapeDirectories = function (string) {
+	return string.replace(/\//g, "\\/");
+};
+
+var checkForImports = function (src) {
+	var contents;
+	var importRegEx = new RegExp("@import");
+	var matchingFiles = [];
+	src.forEach(function (cur, index, src) {
+		contents = fs.readFileSync(cur, encoding);
+		if (importRegEx.test(contents) && matchingFiles.indexOf(cur) === -1) {
+			matchingFiles.push(cur);
 		}
+	});
+	return matchingFiles;
+};
+
+var buildRelPaths = function (from, to, ext) {
+	var curPath = path.relative(from, to).replace('../','').replace('/_','/').replace(ext, '')
+
+	if (path.basename(curPath)[0] === "_") {
+		curPath = curPath.replace('_', '');
+	}
+	return curPath;
+};
+
+var readImportStatements = function (file, statements) {
+	var contents = fs.readFileSync(file, encoding);
+	var dependentFiles = [];
+	statements.forEach(function (curStatement, index, array) {
+		curStatement = new RegExp(escapeDirectories(curStatement));
+		if (curStatement.test(contents) && dependentFiles.indexOf(file) === -1) {
+			dependentFiles.push(file);
+		}
+	});
+	return dependentFiles;
+};
+
+module.exports = function (source) {
+	console.log('Checking ' + chalk.cyan(source));
+	// Initial file checks -- does exist?.
+	if (!fs.existsSync(source)) {
+		console.log('1');
 		return false;
 	}
-	// Check if file is a *.css file, SASS doesn't import those
-	if (path.extname(needle) === '.css') {
-		if (isVerbose) {
-			console.log(chalk.cyan('SASS won\'t import *.css files'));
-		}
+	// Initial file checks -- is css?
+	if (path.extname(source) === '.css') {
+		console.log('2');
 		return false;
 	}
 
-	//Look for verbose for logging options
-	// We have passed our base checks, lets define some variables.
+	// Okay, the basics are there. Lets build the patterns
+	var fileExt = path.extname(source);
+	var fileSearch = glob.sync('**/*' + fileExt);
+	var filesWithImports = checkForImports(fileSearch);
+	var hasPartiasl = true;
+	var allRelPaths = [];
+	var allDependentFiles = [];
 
-	var dependentFiles = [],
-	    ext            = path.extname(needle);
+	console.log(chalk.cyan('These files have import statements:'));
+	console.log(filesWithImports);
+	console.log();
 
-	// If a hastack wasn't sent in, then we define it here. We also remove the needle from the haystack.
-	if (haystack === undefined) {
-		haystack = glob.sync('**/*' + ext);
-	}
-	haystack = _.without(haystack, needle);
-
-	_.each(haystack, function(straw, index, array) {
-		var thisContents, relPath, isDependent, isPartial;
-		thisContents = fs.readFileSync(straw, {encoding: 'utf8'});
-		relPath = path.relative(straw, needle).replace('../', '').replace('/_','/').replace(ext, '');
-		if (relPath[0] === '_') {
-			relPath = relPath.replace('_','');
-		}
-		isDependent = thisContents.indexOf(relPath) !== -1 ? true : false;
-		if (isDependent) {
-			isPartial = path.basename(straw)[0] === '_' ? true : false;
-			if (isPartial) {
-				if (isVerbose) {
-					console.log(chalk.cyan(path.basename(needle)), chalk.white(' is a partial, adding it to the haystack'));
-				}
-				array.push(needle);
-			} else {
-				dependentFiles.push(straw);
-			}
+	// We have files with import statements, now lets build relPaths to check.
+	filesWithImports.forEach(function (cur, index, filesWithImports) {
+		var relPaths = buildRelPaths(cur, source, fileExt);
+		if (relPaths.length > 0 && allRelPaths.indexOf(relPaths) === -1) {
+			allRelPaths.push(relPaths)
 		}
 	});
 
+	console.log(chalk.cyan('These are the relative paths:'));
+	console.log(allRelPaths);
+	console.log();
 
-	if(dependentFiles.length === 0) {
-		if (isVerbose) {
-			console.log(chalk.cyan(path.basename(needle)), chalk.white(' does not have any dependencies'));
-		}
-		return false;
+
+	// We have relPaths, lets check the files with import statements.
+	filesWithImports.forEach(function (cur, index, filesWithImports) {
+		var push = readImportStatements(cur, allRelPaths);
+		allDependentFiles.push(push);
+	});
+
+	allDependentFiles = _.flatten(allDependentFiles);
+
+	if (allDependentFiles.length > 0) {
+		console.log(chalk.cyan('These are all the dependentFiles:'));
+		console.log(allDependentFiles);
+		console.log();
+		return allDependentFiles;
 	}
-	if (isVerbose) {
-		console.log('Dependencies: ' + chalk.cyan(dependentFiles.join(', ')));
+}
+
+var args = process.argv;
+var encoding = {encoding: 'utf8'};
+var fileRegEx = /--file=[a-zA-Z0-9.-_\/]+/;
+var files = [];
+
+args.forEach(function (cur, index, args) {
+	if (fileRegEx.test(cur)) {
+		var file = cur.split('=').pop();
+		files.push(file);
 	}
-	var timerEnd = Date.now();
-	if (isVerbose) {
-		console.log('sass-get-dependencies took: ' + chalk.cyan((timerEnd - timerStart) + 'ms'));
-	}
-	return dependentFiles;
-};
+});
+
+files.forEach(function (cur, index, files) {
+	module.exports(cur);
+});
+var endTime = Date.now();
+
+console.log(endTime - startTime + 'ms to complete');
---
+++ tests/test.js
@@ -0,0 +1,82 @@
+'use strict';
+var assert = require('assert');
+var expect = require('expect.js');
+var dependsOn = require("../");
+
+var partials = 'tests/fixtures/partials';
+var fixtures = 'tests/fixtures';
+
+var notCreated = partials + '/not_real.scss';
+var css = partials + '/_dependentFiles_test_css.css';
+
+var scss = partials + '/_dependentFiles_test_scss.scss';
+var sass = partials + '/_dependentFiles_test_sass.sass';
+var scssTwoDeep = partials + '/_import_into_partial_scss.scss';
+var multipleFiles = partials + '/_import_in_two_files.scss';
+
+describe('sass-get-dependencies', function () {
+	it('fails when files don\'t exist', function () {
+		var actual = dependsOn(notCreated);
+		expect(actual).to.be(false);
+	});
+
+	it('doesn\'t test *.css files', function () {
+		var actual = dependsOn(css);
+		expect(actual).to.be(false);
+	});
+
+	it('reads files', function () {
+		var actual = dependsOn(scss);
+		var expected = ['tests/fixtures/import_partial_scss.scss'];
+
+		assert.deepEqual(actual, expected);
+	});
+
+	it('returns when partials import partias', function () {
+		var actual = dependsOn(scssTwoDeep);
+		var expected =  ['tests/fixtures/partials/_partial_importing_partial_scss.scss' ];
+
+		assert.deepEqual(actual, expected);
+	});
+
+	it('returns multiple files when partials are required in them.', function () {
+		var actual = dependsOn(multipleFiles);
+		var expected = ['tests/fixtures/import_across_2_files_1-2.scss', 'tests/fixtures/import_across_2_files_2-2.scss'];
+
+		assert.deepEqual(actual, expected);
+	});
+});
+
+
+
+// describe('sass-get-dependencies', function () {
+// 	it('returns files dependent on scss files', function () {
+// 		var actual = dependsOn(scss);
+// 		var expected = [
+// 			fixtures + '/import_partial_scss.scss'
+// 		];
+// 		assert.deepEqual(actual, expected);
+// 	});
+
+// 	it('returns files dependent on sass files', function () {
+// 		var actual = dependsOn(sass);
+// 		var expected = [
+// 			fixtures + '/import_partial_sass.sass'
+// 		];
+// 		assert.deepEqual(actual, expected);
+// 	});
+
+// 	it('does not process css files', function () {
+// 		var actual = dependsOn(css);
+// 		var expected = false;
+// 		expect(actual).to.be(false);
+// 	});
+
+// 	it('finds when partials require partials', function () {
+// 		var actual = dependsOn(scssTwoDeep);
+// 		var expected = [
+// 			fixtures + '/partial_two_deep_scss.scss'
+// 		];
+// 		assert.deepEqual(actual, expected);
+// 	});
+// });
