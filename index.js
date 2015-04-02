cd3dad92d588960a0f064795004e73c086964886
Tom Bremer <tom@tbremer.com>
2015-04-02 00:33:09 -0500

    Add editorconfig and reformat some files

--- index.js
+++ index.js
@@ -7,67 +7,67 @@ var nopt = require("nopt");
 var path = require('path');
 var verbose = require('verboser');
 var knownOpts = {
-	"file": String
+  "file": String
 };
 var args = nopt(knownOpts);
 var encoding = {encoding: 'utf8'};
 
 var checkForImports = function (src) {
-	var contents;
-	var importRegEx = new RegExp("@import");
-	var matchingFiles = [];
-	src.forEach(function (cur) {
-		contents = fs.readFileSync(cur, encoding);
-		if (importRegEx.test(contents) && matchingFiles.indexOf(cur) === -1) {
-			matchingFiles.push(cur);
-		}
-	});
-	return matchingFiles;
+  var contents;
+  var importRegEx = new RegExp("@import");
+  var matchingFiles = [];
+  src.forEach(function (cur) {
+    contents = fs.readFileSync(cur, encoding);
+    if (importRegEx.test(contents) && matchingFiles.indexOf(cur) === -1) {
+      matchingFiles.push(cur);
+    }
+  });
+  return matchingFiles;
 };
 
 var readImportStatements = function (file, statements) {
-	var contents;
-	var matchingFiles = [];
-	var basename = path.basename(file, (path.extname(file)));
-	var fileName = (basename[0] === '_') ? basename.substring(1, basename.length) : basename;
-	var importRegEx = new RegExp('@import[\\s]+([\'|"./\\w]+?)' + fileName + '[\'|"];?');
+  var contents;
+  var matchingFiles = [];
+  var basename = path.basename(file, (path.extname(file)));
+  var fileName = (basename[0] === '_') ? basename.substring(1, basename.length) : basename;
+  var importRegEx = new RegExp('@import[\\s]+([\'|"./\\w]+?)' + fileName + '[\'|"];?');
 
-	statements.forEach(function (cur) {
-		contents = fs.readFileSync(cur, encoding);
-		if (importRegEx.test(contents) == true) {
-			matchingFiles.push(cur);
-		}
-	});
-	return matchingFiles;
+  statements.forEach(function (cur) {
+    contents = fs.readFileSync(cur, encoding);
+    if (importRegEx.test(contents) == true) {
+      matchingFiles.push(cur);
+    }
+  });
+  return matchingFiles;
 };
 
 module.exports = function (source) {
-	verbose.log('Checking ' + source).linebreak();
+  verbose.log('Checking ' + source).linebreak();
 
-	// Initial file checks -- does exist?.
-	if (!fs.existsSync(source)) {
-		return [];
-	}
-	// Initial file checks -- is css?
-	if (path.extname(source) === '.css') {
-		return [];
-	}
+  // Initial file checks -- does exist?.
+  if (!fs.existsSync(source)) {
+    return [];
+  }
+  // Initial file checks -- is css?
+  if (path.extname(source) === '.css') {
+    return [];
+  }
 
-	// Okay, the basics are there. Lets build the patterns
-	var fileExt = path.extname(source);
-	var fileSearch = glob.sync('**/*' + fileExt);
-	var filesWithImports = checkForImports(fileSearch);
-	var allRelPaths = [];
-	var allDependentFiles = [];
+  // Okay, the basics are there. Lets build the patterns
+  var fileExt = path.extname(source);
+  var fileSearch = glob.sync('**/*' + fileExt);
+  var filesWithImports = checkForImports(fileSearch);
+  var allRelPaths = [];
+  var allDependentFiles = [];
 
-	verbose.log('These files have import statements:');
-	verbose.log(filesWithImports).linebreak();
+  verbose.log('These files have import statements:');
+  verbose.log(filesWithImports).linebreak();
 
-	allDependentFiles = readImportStatements(source, filesWithImports);
+  allDependentFiles = readImportStatements(source, filesWithImports);
 
-	verbose.log('These are all the dependentFiles:');
-	verbose.log(allDependentFiles);
-	return allDependentFiles;
+  verbose.log('These are all the dependentFiles:');
+  verbose.log(allDependentFiles);
+  return allDependentFiles;
 };
 
 endTime = Date.now();
--- tests/test.js
+++ tests/test.js
@@ -14,38 +14,38 @@ var scssTwoDeep = partials + '/_import_into_partial_scss.scss';
 var multipleFiles = partials + '/_import_in_two_files.scss';
 
 describe('sass-get-dependents', function () {
-	it('fails when files don\'t exist', function () {
-		var actual = dependsOn(notCreated);
-		var expected = [];
+  it('fails when files don\'t exist', function () {
+    var actual = dependsOn(notCreated);
+    var expected = [];
 
-		assert.deepEqual(actual, expected);
-	});
+    assert.deepEqual(actual, expected);
+  });
 
-	it('doesn\'t test *.css files', function () {
-		var actual = dependsOn(css);
-		var expected = [];
+  it('doesn\'t test *.css files', function () {
+    var actual = dependsOn(css);
+    var expected = [];
 
-		assert.deepEqual(actual, expected);
-	});
+    assert.deepEqual(actual, expected);
+  });
 
-	it('reads files', function () {
-		var actual = dependsOn(scss);
-		var expected = ['tests/fixtures/import_partial_scss.scss'];
+  it('reads files', function () {
+    var actual = dependsOn(scss);
+    var expected = ['tests/fixtures/import_partial_scss.scss'];
 
-		assert.deepEqual(actual, expected);
-	});
+    assert.deepEqual(actual, expected);
+  });
 
-	it('returns when partials import partials', function () {
-		var actual = dependsOn(scssTwoDeep);
-		var expected =  ['tests/fixtures/partials/_partial_importing_partial_scss.scss' ];
+  it('returns when partials import partials', function () {
+    var actual = dependsOn(scssTwoDeep);
+    var expected =  ['tests/fixtures/partials/_partial_importing_partial_scss.scss' ];
 
-		assert.deepEqual(actual, expected);
-	});
+    assert.deepEqual(actual, expected);
+  });
 
-	it('returns multiple files when partials are required in them.', function () {
-		var actual = dependsOn(multipleFiles);
-		var expected = ['tests/fixtures/import_across_2_files_1-2.scss', 'tests/fixtures/import_across_2_files_2-2.scss'];
+  it('returns multiple files when partials are required in them.', function () {
+    var actual = dependsOn(multipleFiles);
+    var expected = ['tests/fixtures/import_across_2_files_1-2.scss', 'tests/fixtures/import_across_2_files_2-2.scss'];
 
-		assert.deepEqual(actual, expected);
-	});
+    assert.deepEqual(actual, expected);
+  });
 });
